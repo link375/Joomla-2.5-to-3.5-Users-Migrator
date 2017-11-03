ed662fc2ba7aafff9b5208540e64d4c69049bdab
link375 <https://github.com/link375>
2017-11-03 09:35:25 -0600

    Renamed Project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SQL_Table_Differences
+# Joomla 2.5 - 3.5 User Migrator
  This program will help you merge a joomla 2.5 users table with a joomla 3.5 users table
  the result will be written to the joomla 3.5 table all usernames and emails will be checked
  there will not be any overlapping results new entries will be written with auto-incrementing ids
--- main.py
+++ main.py
@@ -3,21 +3,6 @@ import csv
 import functions as utilities
 import sqlQueries as Query
 
-# This program will help you merge
-# a joomla 2.5 users table with a joomla 3.5 users table
-# the result will be written to the joomla 3.5 table
-# all usernames and emails will be checked
-# there will not be any overlapping results
-# new entries will be written with auto-incrementing ids
-
-# instructions #
-# MAKE A BACKUP OF YOUR 2 DATABASES! DON'T BE STUPID!
-# setup the db.py file to match your 2 databases
-# check the sqlQueries.py file to make sure you are querying the correct tables
-# check the sqlQueries.py file to make sure you are INSERTING into the correct table
-# run
-# profit
-
 # convert tuples to lists so they can be modified
 firstDB_list = list(Query.results_firstDB)
 secondDB_list = list(Query.results_secondDB)
