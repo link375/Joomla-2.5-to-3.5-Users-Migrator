69c6857608399e53b76369510528b9c2b258c50c
link375 <https://github.com/link375>
2018-02-16 13:02:22 -0700

    added destination db info and changed db2 variables

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 import MySQLdb
 
-# DATABASE INFORMATION
-host = "your_host.com"
-user = "your_db_user"
-password = "your_db_password"
+# ORIGINAL DATABASE INFO
+host = "your_joomla2.5_host"
+user = "your_joomla2.5_db_user"
+password = "your_joomla2.5_db_password"
 
 # ORIGINAL DATABASE - joomla 2.5
 joomlaTwoDB = "your_joomla_2.5_schema"
@@ -19,14 +19,19 @@ firstDB = MySQLdb.connect(host=host,
 firstDB_c = firstDB.cursor()
 
 
+# DESTINATION DATABASE INFO
+host2 = "your_joomla3.5_db_host"
+user2 = "your_joomla3.5_db_user"
+password2 = "your_joomla3.5_db_password"
+
 # SECOND DATABASE - joomla 3.5
 JoomlaThreeDB = "your_joomla_3.5_schema"
 
 # connect to the db
 # joomla 3.5
-secondDB = MySQLdb.connect(host=host,
-                           user=user,
-                           passwd=password,
+secondDB = MySQLdb.connect(host=host2,
+                           user=user2,
+                           passwd=password2,
                            db=JoomlaThreeDB)
 
 # create a cursor
@@ -35,11 +40,14 @@ secondDB_c = secondDB.cursor()
 
 #########TESTING##########
 
-joomlaTestDB = "your_test_schema"
+# host, user, passwd should be the same as destination DB
+# if not then add the host, user, password like the dbs above
 
-testDB = MySQLdb.connect(host=host,
-                         user=user,
-                         passwd=password,
+joomlaTestDB = "your_destination_schema"
+
+testDB = MySQLdb.connect(host=host2,
+                         user=user2,
+                         passwd=password2,
                          db=joomlaTestDB)
 
 test_c = testDB.cursor()
