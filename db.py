53460a9e1c6b571a398ab42c8241d5bb74f0cb9e
link375 <https://github.com/link375>
2018-02-16 11:57:10 -0700

    Reorganized the db.py file into db groups

--- db.py
+++ db.py
@@ -8,10 +8,6 @@ password = "your_db_password"
 # ORIGINAL DATABASE - joomla 2.5
 joomlaTwoDB = "your_joomla_2.5_schema"
 
-# SECOND DATABASE - joomla 3.5
-JoomlaThreeDB = "your_joomla_3.5_schema"
-
-
 # connect to the db
 # joomla 2.5
 firstDB = MySQLdb.connect(host=host,
@@ -19,15 +15,21 @@ firstDB = MySQLdb.connect(host=host,
                           passwd=password,
                           db=joomlaTwoDB)
 
+# create a cursor
+firstDB_c = firstDB.cursor()
+
+
+# SECOND DATABASE - joomla 3.5
+JoomlaThreeDB = "your_joomla_3.5_schema"
+
+# connect to the db
 # joomla 3.5
 secondDB = MySQLdb.connect(host=host,
                            user=user,
                            passwd=password,
                            db=JoomlaThreeDB)
 
-
 # create a cursor
-firstDB_c = firstDB.cursor()
 secondDB_c = secondDB.cursor()
 
 
